de5cb5a5c08ea19cefa94e7525091d67951263b0
Vibhore Srivastava <vibhoresrivastava@Vibhores-MacBook-Air.local>
2024-10-26 00:45:42 +0530

    rest template set up

--- src/main/java/com/pureandcold/aggregator/services/external/InventoryService.java
+++ src/main/java/com/pureandcold/aggregator/services/external/InventoryService.java
@@ -1,8 +1,25 @@
 package com.pureandcold.aggregator.services.external;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class InventoryService {
+    private final RestTemplate restTemplate;
 
+    public InventoryService(@Qualifier("inventoryRestTemplate") RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getFetchProductResponse(String request) {
+        String uri = "uri";
+        try {
+            //restTemplate.exchange(uri, request, HttpMethod.GET, String.class);
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
+    }
 }
